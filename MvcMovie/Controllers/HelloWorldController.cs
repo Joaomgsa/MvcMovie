eeb0bc8a1c504c578205875d0cefcd2df4e7ec0d
João Santos <joaomgsa@gmail.com>
2021-12-07 19:32:07 -0300

    Adicionar arquivos de projeto.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        //
+        // get : HelloWorld
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // get : Welcome()
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "olá" + name;
+            ViewData["NumTimes"] = numTimes;
+           
+            return View();
+            
+        }
+    }
+}
